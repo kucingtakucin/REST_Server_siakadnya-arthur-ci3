506200f2883858c2f06265a628ff8123c6fcdc74
Adam Arthur Faizal <adam.faizal.af6@gmail.com>
2020-11-10 11:53:56 +0700

    Menyelesaikan rest server sederhana

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -4,11 +4,14 @@ use chriskacerguis\RestServer\RestController;
 
 class Api extends RestController {
 
+    /**
+     * Api constructor.
+     */
     public function __construct()
     {
         // Construct the parent class
         parent::__construct();
-        $this->load('Mahasisa');
+        $this->load->model('Mahasiswa');
     }
 
     public function users_get()
@@ -38,6 +41,101 @@ class Api extends RestController {
                 'status' => false,
                 'message' => 'No such user found'
             ], 404 );
+        }
+    }
+
+    /**
+     *
+     */
+    public function students_get()
+    {
+        $id = $this->get('id');
+        if ($id === null) {
+            $mahasiswa = $this->Mahasiswa->get();
+        } else {
+            $mahasiswa = $this->Mahasiswa->get($id);
+        }
+        if ($mahasiswa) {
+            $this->response([
+                'status' => true,
+                'data' => $mahasiswa
+            ], 200);
+        } else {
+            $this->response([
+                'status' => false,
+                'message' => 'No such user found'
+            ], 404);
+        }
+    }
+
+    public function students_post()
+    {
+        $data = [
+            'nama' => $this->post('nama'),
+            'nim' => $this->post('nim'),
+            'jurusan' => $this->post('jurusan'),
+            'angkatan' => $this->post('angkatan'),
+            'foto' => $this->post('foto')
+        ];
+
+        if ($this->Mahasiswa->create($data) > 0) {
+            $this->response([
+                'status' => true,
+                'message' => 'New mahasiwa has been created'
+            ], 201);
+        } else {
+            $this->response([
+                'status' => false,
+                'message' => 'Failed to created data'
+            ], 400);
+        }
+    }
+
+    public function students_put()
+    {
+        $id = $this->put('id');
+        $data = [
+            'nama' => $this->put('nama'),
+            'nim' => $this->put('nim'),
+            'jurusan' => $this->put('jurusan'),
+            'angkatan' => $this->put('angkatan'),
+            'foto' => $this->put('foto')
+        ];
+
+        if ($this->Mahasiswa->update($data, $id) > 0) {
+            $this->response([
+                'status' => true,
+                'message' => 'New mahassiwa has been updated.'
+            ], 204);
+        } else {
+            $this->response([
+                'status' => false,
+                'message' => 'Failed to update data!',
+            ], 400);
+        }
+    }
+
+    public function students_delete()
+    {
+        $id = $this->delete('id');
+        if ($id === null) {
+            $this->response([
+                'status' => false,
+                'message' => 'provide an id!',
+            ], 400);
+        } else {
+            if ($this->Mahasiswa->delete($id) > 0) {
+                $this->response([
+                    'status' => true,
+                    'id' => $id,
+                    'message' => 'deleted.'
+                ], 204);
+            } else {
+                $this->response([
+                    'status' => false,
+                    'message' => 'id not found!'
+                ], 400);
+            }
         }
     }
 }
--- application/models/Mahasiswa.php
+++ application/models/Mahasiswa.php
@@ -3,8 +3,46 @@
 
 class Mahasiswa extends CI_Model
 {
-    public function get()
+    /**
+     * @param null $id
+     * @return mixed
+     */
+    public function get($id = null)
     {
-        return $this->db->get("mahasiswa");
+        if ($id === null) {
+            return $this->db->get("mahasiswa")->result_array();
+        }
+        return $this->db->get_where('mahasiswa', ['id' => $id])->result_array();
+    }
+
+    /**
+     * @param $id
+     * @return mixed
+     */
+    public function delete($id)
+    {
+        $this->db->delete('mahasiswa',['id' => $id]);
+        return $this->db->affected_rows();
+    }
+
+    /**
+     * @param $data
+     * @return mixed
+     */
+    public function create($data)
+    {
+        $this->db->insert('mahasiswa', $data);
+        return $this->db->affected_rows();
+    }
+
+    /**
+     * @param $data
+     * @param $id
+     * @return mixed
+     */
+    public function update($data, $id)
+    {
+        $this->db->update('mahasiswa', $data, ['id' => $id]);
+        return $this->db->affected_rows();
     }
 }
